82db324fd73fdecce325a55a9e0bb599dce8ea68
aatndev <ksaadithyan@gmail.com>
2026-06-17 14:25:25 +0530

    New Tab/navbar section;

--- script.js
+++ script.js
@@ -42,4 +42,12 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     }
+
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+
+    hamburger.addEventListener("click", () => {
+        menu.classList.toggle("active");
+    });
+
 });
